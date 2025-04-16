b11a073920008db478333992d112e0ef6e20b996
José Reis <diosep70@gmail.com>
2025-04-16 10:06:45 -0300

    cadastro e editar do usuario

--- gym_genesis-main/codigo/public/funcoes.php
+++ gym_genesis-main/codigo/public/funcoes.php
@@ -1,2 +1,21 @@
 <?php
+
+function cadatrarUsuario($conexao, $nome, $senha, $email, $cpf, $data_nasc, $telefone, $foto_perfil, $numero_matricula, $tipo){
+    $sql = 'INSERT INTO usuario (nome, senha, email, cpf, data_de_nascimento, telefone, foto_de_perfil, numero_matricula, tipo_usuario) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    $comando = mysqli_prepare($conexao, $sql);
     
+    mysqli_stmt_bind_param($comando, 'ssssssssi', $nome, $senha, $email, $cpf, $data_nasc, $telefone, $foto_perfil, $numero_matricula, $tipo);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+    mysqli_stmt_close($comando);
+    return $funcionou;
+}
+function editarUsuario($conexao, $idusuario, $nome, $senha, $email, $cpf, $data_de_nascimento, $telefone, $foto_de_perfil, $numero_matricula, $tipo_usuario){
+    $sql = '';
+    $comando = mysqli_prepare($conexao ,$sql);
+    mysqli_stmt_bind_param($comando, 'ssssssssi', $nome, $senha, $email, $cpf, $data_nasc, $telefone, $foto_perfil, $numero_matricula, $tipo);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+    mysqli_stmt_close($comando);
+    return $funcionou;
+    }
