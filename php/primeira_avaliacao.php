7b69e617832833c67e9b8aa903b066268da7e94e
Kingnike1 <161983544+Kingnike1@users.noreply.github.com>
2025-07-23 16:55:24 -0300

    Update primeira_avaliacao.php

--- php/primeira_avaliacao.php
+++ php/primeira_avaliacao.php
@@ -1,19 +1,27 @@
 <?
 
 require_once '../code/funcao.php';
-$idusuario = $_SESSION['idusuario'];
+$idusuario = $_POST['idusuario'];
 if (!$idusuario) {
     die(json_encode(['error' => 'ID do usuário não fornecido']));
 }
 $peso = $_POST['peso']; 
 $altura = $_POST['altura']; 
 $objetivo = $_POST['objetivo'];
-$meta = $_POST['meta'];
+$percentual_gordura = $_POST['percentual_gordura'];
 $dia_semana = $_POST['dias_semana'];
 $horario_preferido = $_POST['horario_preferido'];
+$data_avaliacao = $_POST['data_avaliacao'] ?? date('Y-m-d');
+
+// Valida formato da data
+if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $data_avaliacao)) {
+    $data_avaliacao = date('Y-m-d'); // fallback
+}
 
 
-$imc = $peso / ($altura ** 2);
+
+$imc = $altura > 0 ? $peso / ($altura ** 2) : 0;
 
 
-cadastrarAvaliacaoFisica($peso, $altura, $imc, $objetivo, $meta, $dia_semana);
+$resposta = cadastrarAvaliacaoFisica($peso, $altura, $imc, $percentual_gordura, $data_avaliacao, $idusuario);
+
