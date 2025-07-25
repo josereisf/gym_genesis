40bcbb99c1569ad2044ac69c4502168c27341461
Kingnike1 <161983544+Kingnike1@users.noreply.github.com>
2025-07-25 10:44:00 -0300

    Update teste_consulta_usuario.php

--- code/tests/teste_consulta_usuario.php
+++ code/tests/teste_consulta_usuario.php
@@ -3,5 +3,5 @@ require_once '../funcao.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
-$data = listarUsuarioCompleto(9);
+$data = listarUsuarioCompleto(1);
 echo json_encode($data, JSON_UNESCAPED_UNICODE);
