e058b1a04b6c2944a9a5d99c5d0151adfc8ef992
Kingnike1 <161983544+Kingnike1@users.noreply.github.com>
2025-07-25 10:43:57 -0300

    Update teste_atualizarFotoPerfil.php

--- code/tests/teste_atualizarFotoPerfil.php
+++ code/tests/teste_atualizarFotoPerfil.php
@@ -2,7 +2,7 @@
 
 require_once '../funcao.php';
 
-$idusuario = 4;
-$foto = $_FILES['foto'] ?? 123412344;
+$idusuario = 1;
+$foto = $_FILES['foto'] ?? "pessoas-mulheres-negocios-e-conceito-de-retrato-rosto-de-jovem-sorridente-feliz_380164-121867.avif";
 
 var_dump(atualizarFotoUsuario($foto, $idusuario));
