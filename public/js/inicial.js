e30b93d605a348f414b58a4f73b56839c92da1f6
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2025-10-01 19:55:00 -0300

    criação do identificador de Navegador

--- public/js/inicial.js
+++ public/js/inicial.js
@@ -1,3 +1,19 @@
+var sBrowser,
+  sUsrAg = navigator.userAgent;
+
+if (sUsrAg.indexOf("Chrome") > -1) {
+  sBrowser = "Google Chrome";
+} else if (sUsrAg.indexOf("Safari") > -1) {
+  sBrowser = "Apple Safari";
+} else if (sUsrAg.indexOf("Opera") > -1) {
+  sBrowser = "Opera";
+} else if (sUsrAg.indexOf("Firefox") > -1) {
+  sBrowser = "Mozilla Firefox";
+} else if (sUsrAg.indexOf("MSIE") > -1) {
+  sBrowser = "Microsoft Internet Explorer";
+}
+
+
 // Criar partículas para o fundo
 function createParticles() {
   const container = document.getElementById("particles");
