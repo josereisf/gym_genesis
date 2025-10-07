3f3393be1c7aa2fd5e55d8fde9062fc7419c61e1
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2025-10-07 16:03:38 -0300

    Update forum.php

--- public/forum.php
+++ public/forum.php
@@ -9,7 +9,7 @@ require_once __DIR__ . "/../code/funcao.php";
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Fórum Premium - Gym Gênesis</title>
+  <title>Fórum - Gym Gênesis</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
